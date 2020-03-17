1302371d3d4eeb4cd416fb6c7a190f253e035d93
dineshp4308 <46320698+dineshp4308@users.noreply.github.com>
2020-03-17 16:03:09 +0530

    Test.java

---
+++ Test1.java
@@ -0,0 +1,13 @@
+import java.util.*
+
+public class Test1
+{
+public static void main(String [] args)
+{
+int a = 10;
+int b = 66;
+
+System.out.println("add = "+(a+b));
+System.out.println("sub = "+ (a-b));
+}
+}
